aa5033811a3c10aae58fa868cdd4894df8c9b497
Cucu <cucu.szpem@gmail.com>
2013-10-26 20:23:11 +0200

    Comments, Topic, Poll added to models

--- ktapp/models.py
+++ ktapp/models.py
@@ -7,6 +7,7 @@ class Film(models.Model):
     other_titles = models.TextField(blank=True)
     year = models.PositiveIntegerField(default=0)
     plot_summary = models.TextField(blank=True)
+    last_comment = models.ForeignKey("Comment", blank=True, null=True, related_name="last_film_comment")
     
     def __unicode__(self):
         return self.orig_title + " [" + unicode(self.year) + "]"
@@ -27,3 +28,45 @@ class Vote(models.Model):
     
     class Meta:
         unique_together = ("film", "user")
+
+
+class Comment(models.Model):
+    DOMAIN_FILM = "F"
+    DOMAIN_TOPIC = "T"
+    DOMAIN_POLL = "P"
+    DOMAINS = [
+        (DOMAIN_FILM, "Film"),
+        (DOMAIN_TOPIC, "Topic"),
+        (DOMAIN_POLL, "Poll"),
+    ]
+    domain = models.CharField(max_length=1, choices=DOMAINS, default=DOMAIN_FILM)
+    film = models.ForeignKey(Film, blank=True, null=True)
+    topic = models.ForeignKey("Topic", blank=True, null=True)
+    poll = models.ForeignKey("Poll", blank=True, null=True)
+    created_by = models.ForeignKey(User)
+    created_at = models.DateTimeField(auto_now_add=True)
+    content = models.TextField(blank=True)
+    reply_to = models.ForeignKey("self", blank=True, null=True)
+    
+    def __unicode__(self):
+        return self.content[:100]
+
+
+class Topic(models.Model):
+    title = models.CharField(max_length=200)
+    number_of_comments = models.PositiveIntegerField(default=0)
+    created_by = models.ForeignKey(User)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_comment = models.ForeignKey(Comment, blank=True, null=True, related_name="last_topic_comment")
+    
+    def __unicode__(self):
+        return self.title
+
+
+class Poll(models.Model):
+    title = models.CharField(max_length=200)
+    # NOTE: this is just a placeholder now (for Comment)
+    # TODO: extend later with real content
+    
+    def __unicode__(self):
+        return self.title
