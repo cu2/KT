fb4305c8cba8d84c5b0b5e05ccc961f1ccfbe210
Andras Nemeth <cucu.szpem@gmail.com>
2015-08-14 15:17:41 +0200

    [settings] media from static.kritikustomeg.org

--- kt/settings.py
+++ kt/settings.py
@@ -63,7 +63,7 @@ MEDIA_ROOT = 'ktapp/media/'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = '/media/'
+MEDIA_URL = 'http://static.kritikustomeg.org/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
