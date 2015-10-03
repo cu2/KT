e7b5aa2a8998253665d49bb15ff934efec90e6e0
Andras Nemeth <cucu.szpem@gmail.com>
2015-10-03 12:30:10 +0200

    [film of the day] deployed

--- ktapp/views/web_views.py
+++ ktapp/views/web_views.py
@@ -29,18 +29,7 @@ MINIMUM_YEAR = 1920
 def index(request):
     hash_of_the_day = int(hashlib.md5(datetime.datetime.now().strftime('%Y-%m-%d')).hexdigest(), 16)
     # film of the day
-    film_of_the_day_qs = models.Film.objects.filter(
-        main_poster__isnull=False,
-        number_of_ratings__gte=10,
-        average_rating__gte=4,
-        number_of_comments__gte=3,
-    )
-    number_of_films = film_of_the_day_qs.count()  # cca 1600
-    film_no_of_the_day = hash_of_the_day % number_of_films
-    try:
-        film_of_the_day = film_of_the_day_qs.order_by('id')[film_no_of_the_day]
-    except models.Film.DoesNotExist:
-        film_of_the_day = models.Film.objects.get(id=1)
+    film_of_the_day = models.OfTheDay.objects.filter(domain='F').order_by('-day')[0].film
     # toplist of the day
     number_of_toplists = models.UserToplist.objects.filter(quality=True).count()
     toplist_no_of_the_day = hash_of_the_day % number_of_toplists
