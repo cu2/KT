f272dd2567c3e3aff99bb28132d5f6b56f437fc3
Andras Nemeth <cucu.szpem@gmail.com>
2015-11-12 23:59:22 +0100

    [models.UserUserRating] remove 25 columns

---
+++ ktapp/migrations/0045_auto_20151112_0140.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ktapp', '0044_useruserrating_similarity'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='useruserrating',
+            name='number_of_ratings_11',
+        ),
+        migrations.RemoveField(
+            model_name='useruserrating',
+            name='number_of_ratings_12',
+        ),
+        migrations.RemoveField(
+            model_name='useruserrating',
+            name='number_of_ratings_13',
+        ),
+        migrations.RemoveField(
+            model_name='useruserrating',
+            name='number_of_ratings_14',
+        ),
+        migrations.RemoveField(
+            model_name='useruserrating',
+            name='number_of_ratings_15',
+        ),
+        migrations.RemoveField(
+            model_name='useruserrating',
+            name='number_of_ratings_21',
+        ),
+        migrations.RemoveField(
+            model_name='useruserrating',
+            name='number_of_ratings_22',
+        ),
+        migrations.RemoveField(
+            model_name='useruserrating',
+            name='number_of_ratings_23',
+        ),
+        migrations.RemoveField(
+            model_name='useruserrating',
+            name='number_of_ratings_24',
+        ),
+        migrations.RemoveField(
+            model_name='useruserrating',
+            name='number_of_ratings_25',
+        ),
+        migrations.RemoveField(
+            model_name='useruserrating',
+            name='number_of_ratings_31',
+        ),
+        migrations.RemoveField(
+            model_name='useruserrating',
+            name='number_of_ratings_32',
+        ),
+        migrations.RemoveField(
+            model_name='useruserrating',
+            name='number_of_ratings_33',
+        ),
+        migrations.RemoveField(
+            model_name='useruserrating',
+            name='number_of_ratings_34',
+        ),
+        migrations.RemoveField(
+            model_name='useruserrating',
+            name='number_of_ratings_35',
+        ),
+        migrations.RemoveField(
+            model_name='useruserrating',
+            name='number_of_ratings_41',
+        ),
+        migrations.RemoveField(
+            model_name='useruserrating',
+            name='number_of_ratings_42',
+        ),
+        migrations.RemoveField(
+            model_name='useruserrating',
+            name='number_of_ratings_43',
+        ),
+        migrations.RemoveField(
+            model_name='useruserrating',
+            name='number_of_ratings_44',
+        ),
+        migrations.RemoveField(
+            model_name='useruserrating',
+            name='number_of_ratings_45',
+        ),
+        migrations.RemoveField(
+            model_name='useruserrating',
+            name='number_of_ratings_51',
+        ),
+        migrations.RemoveField(
+            model_name='useruserrating',
+            name='number_of_ratings_52',
+        ),
+        migrations.RemoveField(
+            model_name='useruserrating',
+            name='number_of_ratings_53',
+        ),
+        migrations.RemoveField(
+            model_name='useruserrating',
+            name='number_of_ratings_54',
+        ),
+        migrations.RemoveField(
+            model_name='useruserrating',
+            name='number_of_ratings_55',
+        ),
+    ]
