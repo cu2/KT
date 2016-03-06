a40b3d0a6d2fdadb268260a00a47990bd7df5634
Andras Nemeth <cucu.szpem@gmail.com>
2016-03-06 13:30:08 +0100

    [search] full text index added to users

--- ktapp/helpers/search.py
+++ ktapp/helpers/search.py
@@ -51,19 +51,15 @@ def find_film_by_link(q):
 
 
 def find_artists(q_pieces, limit):
-    artists = models.Artist.objects
-    artists = artists.filter(slug_cache__search=' '.join(['+%s*' % slugify(q_piece) for q_piece in q_pieces]))
-    return artists.order_by('-number_of_ratings')[:limit]
+    return models.Artist.objects.filter(
+        slug_cache__search=' '.join(['+%s*' % slugify(q_piece) for q_piece in q_pieces])
+    ).order_by('-number_of_ratings')[:limit]
 
 
 def find_users(q_pieces, limit):
-    users = models.KTUser.objects
-    for q_piece in q_pieces:
-        users = users.filter(
-            Q(username__icontains=q_piece)
-            | Q(slug_cache__icontains=slugify(q_piece))
-        )
-    return users.order_by('username')[:limit]
+    return models.KTUser.objects.filter(
+        slug_cache__search=' '.join(['+%s*' % slugify(q_piece) for q_piece in q_pieces])
+    ).order_by('username')[:limit]
 
 
 def find_topics(q_pieces, limit):
