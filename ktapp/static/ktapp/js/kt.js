c480d08def2faa1c261463360c4b74f0c78e8d01
Andras Nemeth <cucu.szpem@gmail.com>
2016-03-31 10:07:18 +0200

    [header] mobile search

--- ktapp/static/ktapp/js/kt.js
+++ ktapp/static/ktapp/js/kt.js
@@ -18,9 +18,9 @@ var ktApp = {
         ktApp.lastScrollTop = st;
     },
     resizeSearchAutocomplete: function() {
-        var search_autocomplete_results = $('#search_autocomplete_results');
+        var search_autocomplete_results = $('#search-autocomplete-results');
         if (search_autocomplete_results.css('display') === 'block') {
-            var search_input = $('#search_input');
+            var search_input = $('#search-input');
             var input_offset = search_input.offset();
             var input_dim = {
                 width: search_input.outerWidth(true) + 1,
@@ -35,10 +35,12 @@ var ktApp = {
     },
     searchInputChanged: false,
     getSearchResults: function() {
-        var search_autocomplete_results = $('#search_autocomplete_results');
-        var search_input = $('#search_input');
+        var search_autocomplete_results = $('#search-autocomplete-results');
+        var mobile_search_results = $('#mobile-search-results');
+        var search_input = $('#search-input');
         if (search_input.val().trim().length < 2) {
             search_autocomplete_results.hide();
+            mobile_search_results.html('');
         } else {
             search_input.addClass('loading');
             $.getJSON('/api/autocomplete/search/', {
@@ -61,21 +63,23 @@ var ktApp = {
                             search_autocomplete_results_html += '<ul>';
                             for(var i = 0; i < results.length; i++) {
                                 search_autocomplete_results_html += '<li><a href="' + results[i].url + '">';
-                                search_autocomplete_results_html += '<span class="search_autocomplete_results_thumbnail_container">';
+                                search_autocomplete_results_html += '<span class="search-autocomplete-results-thumbnail-container">';
                                 if (results[i].thumbnail) search_autocomplete_results_html += '<img src="' + results[i].thumbnail + '" />';
                                 search_autocomplete_results_html += '</span>';
                                 search_autocomplete_results_html += results[i].title;
-                                if (results[i].subtitle) search_autocomplete_results_html += '<br /><span class="search_autocomplete_results_subtitle">' + results[i].subtitle + '</span>';
-                                if (results[i].subsubtitle) search_autocomplete_results_html += '<br /><span class="search_autocomplete_results_subtitle">' + results[i].subsubtitle + '</span>';
+                                if (results[i].subtitle) search_autocomplete_results_html += '<br /><span class="search-autocomplete-results-subtitle">' + results[i].subtitle + '</span>';
+                                if (results[i].subsubtitle) search_autocomplete_results_html += '<br /><span class="search-autocomplete-results-subtitle">' + results[i].subsubtitle + '</span>';
                                 search_autocomplete_results_html += '</a></li>';
                             }
                             search_autocomplete_results_html += '</ul>';
                         }
                         search_autocomplete_results.html(search_autocomplete_results_html);
                         search_autocomplete_results.show();
+                        mobile_search_results.html(search_autocomplete_results_html);
                         ktApp.resizeSearchAutocomplete();
                     } else {
                         search_autocomplete_results.hide();
+                        mobile_search_results.html('');
                     }
                 }
             });
@@ -84,7 +88,11 @@ var ktApp = {
 };
 
 $(function() {
-    $('#search_input').on('input propertychange paste', function() {
+    $('#search-input-mobile').on('input propertychange paste', function() {
+        $('#search-input').val($('#search-input-mobile').val());
+        ktApp.searchInputChanged = true;
+    });
+    $('#search-input').on('input propertychange paste', function() {
         ktApp.searchInputChanged = true;
     });
     setInterval(function() {
@@ -127,7 +135,7 @@ $(function() {
         }, 100);
     });
 
-    $('.toggle-hamburger-menu').click(function() {
+    $('.hamburger-menu-open').click(function() {
         ktApp.pageY = $(window).scrollTop();
         $('#hamburger-menu').show();
         $('#hamburger-menu-ul').scrollTop(0).animate({
@@ -137,13 +145,31 @@ $(function() {
         $('html').css('overflowY', 'hidden');
         return false;
     });
-    $('.hamburger-closer').click(function() {
+    $('.hamburger-menu-close').click(function() {
         $('#hamburger-menu-ul').animate({
             left: '-100%',
             right: '100%'
         }, 'fast', function() {
             $('#hamburger-menu').hide();
         });
+        $('html').css('overflowY', 'auto');
+        $(window).scrollTop(ktApp.pageY);
+        return false;
+    });
+
+    $('.mobile-search-open').click(function() {
+        ktApp.pageY = $(window).scrollTop();
+        $('#mobile-search-screen').show();
+        $('#search-input-mobile').focus();
+        $('html').css('overflowY', 'hidden');
+        return false;
+    });
+    $('.mobile-search-close').click(function() {
+        $('#mobile-search-screen').hide();
+        $('#search-input-mobile').blur().val('');
+        $('#search-input').val('');
+        $('#mobile-search-results').html('');
+        ktApp.searchInputChanged = true;
         $('html').css('overflowY', 'auto');
         $(window).scrollTop(ktApp.pageY);
         return false;
