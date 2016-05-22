51d0c8c963db02a2b8bb776343788934ed2b67c7
Andras Nemeth <cucu.szpem@gmail.com>
2016-05-22 18:59:06 +0200

    [deploy] build script updated

--- build_statics.sh
+++ build_statics.sh
@@ -1,16 +1,35 @@
 #!/bin/bash -e
 
-echo '# BUILDING CSS...'
-cd bootstrap
-grunt dist
-cd ..
-cp bootstrap/dist/css/bootstrap.min.css ktapp/static/ktapp/css/kt-bootstrap.min.css
-echo '# CSS BUILT.'
+if [[ $# -lt 1 ]]; then
+  BUILD_CSS=1
+  BUILD_JS=1
+else
+  if [[ "$1" == "js" ]]; then
+    BUILD_CSS=0
+    BUILD_JS=1
+  elif [[ "$1" == "css" ]]; then
+    BUILD_CSS=1
+    BUILD_JS=0
+  else
+    echo "Usage: $0 [js|css]"
+    exit 1
+  fi
+fi
 
-csshash=$(md5 -q ktapp/static/ktapp/css/kt-bootstrap.min.css)
-gsed -i 's/\?csshash\=[^"]*"/\?csshash\='$csshash'"/g' ktapp/templates/ktapp/layout.html
+if [[ $BUILD_CSS -eq 1 ]]; then
+  echo '# BUILDING CSS...'
+  cd bootstrap
+  grunt dist
+  cd ..
+  cp bootstrap/dist/css/bootstrap.min.css ktapp/static/ktapp/css/kt-bootstrap.min.css
+  csshash=$(md5 -q ktapp/static/ktapp/css/kt-bootstrap.min.css)
+  gsed -i 's/\?csshash\=[^"]*"/\?csshash\='$csshash'"/g' ktapp/templates/ktapp/layout.html
+  echo '# CSS BUILT.'
+fi
 
-jshash=$(md5 -q ktapp/static/ktapp/js/kt.js)
-gsed -i 's/\?jshash\=[^"]*"/\?jshash\='$jshash'"/g' ktapp/templates/ktapp/layout.html
-
-echo '# DONE DONE.'
+if [[ $BUILD_JS -eq 1 ]]; then
+  echo '# BUILDING JS...'
+  jshash=$(md5 -q ktapp/static/ktapp/js/kt.js)
+  gsed -i 's/\?jshash\=[^"]*"/\?jshash\='$jshash'"/g' ktapp/templates/ktapp/layout.html
+  echo '# JS BUILT.'
+fi
