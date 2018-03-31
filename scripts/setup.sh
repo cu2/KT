8ea60145e2ce420877239771c90f211e7c1a22c6
Andras Nemeth <cucu.szpem@gmail.com>
2018-03-31 21:11:45 +0200

    [scripts/setup] progress details added

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,12 +4,21 @@ set -o pipefail
 PARENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 
 if [ ! -d "$PARENT_DIR/virtualenv" ]; then
+  echo 'Creating virtualenv...'
   virtualenv "$PARENT_DIR/virtualenv"
+  echo 'Created virtualenv.'
 fi
 
 . "$PARENT_DIR/virtualenv/bin/activate"
 
+echo 'Installing requirements...'
 pip install -r "$PARENT_DIR/requirements.txt"
+echo 'Installed requirements.'
 
+echo 'Initializing database...'
 ./manage.py migrate
+echo 'Initialized database.'
+
+echo 'Loading data...'
 ./manage.py loaddata fixtures/initial_data.json
+echo 'Loaded data.'
